9ffce211589a8f6568cc8fe20eb998f0af0a6635
ouroborus88 <153806123+ouroborus88@users.noreply.github.com>
2026-01-02 00:13:57 +0800

    Update biography text in mock.ts

---
+++ mock/mock/.ts
@@ -0,0 +1,11 @@
+export const data = {
+  name: "Kaun I Chen",
+  job: "Artist",
+  email: "danseenbarbe@gmail.com",
+  instagram_link: "https://www.instagram.com/chloeapplecan/",
+  facebook_link: "https://www.facebook.com/kuanichencan/",
+  aboutme_para1:
+    "Kuan I Chen first bio",
+  aboutme_para2:
+    "second bio",
+};
